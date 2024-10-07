3381e80478cb0620e0d682d142534d31b9fc8c32
Wolfgang Kremer <WKremer@OmS-Corp.OmniSys.de>
2024-10-07 17:40:21 +0200

    register and login functional

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {User} from "../../models/user";
+import {LoginService} from "../../services/login.service";
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/Auth.service";
+import {MatButton} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  user: User = {
+    id: '',
+    username: '',
+    password: ''
+  }
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private authService: AuthService) {
+
+  }
+
+  login() {
+    console.log('login attempt with username: ' + this.user.username);
+    this.authService.login(this.user.username, this.user.password).subscribe({
+      next: (response) => {
+        console.log('login successful, response: ' + response);
+        this.router.navigate(['/table-view']);
+      },
+      error: (error) => {
+        console.error('login failed, error: ' + error);
+      }
+    });
+  }
 }
